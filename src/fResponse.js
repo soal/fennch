c1d1b01377625fde78d72836673da56fbfb8ee2f
soal <sorrow.about.alice@pm.me>
2019-02-04 22:19:57 +0300

    Added initial Response proxy implementation

--- src/fResponse.js
+++ src/fResponse.js
@@ -1,108 +1,46 @@
-async function parseResponse(res, contentType) {
+async function parseResponse(rawResponse) {
   let body = null;
-  let err = null;
-  if (res.ok) {
-    if (contentType && contentType.includes("application/json")) {
-      try {
-        if (typeof res.json === "function") {
-          body = await res.json();
-        } else {
-          body = await res.text();
-          body = JSON.parse(body);
-        }
-      } catch (err) {
-        return err;
-      }
-    } else {
-      body = await res.text();
-    }
-  } else {
-    err = new Error(res.statusText);
+  let error = null;
 
-    // check if the response was JSON, and if so, better the error
-    if (contentType && contentType.includes("application/json")) {
-      try {
-        // // attempt to parse json body to use as error message
-        if (typeof res.json === "function") {
-          body = await res.json();
-        } else {
-          body = await res.text();
-          body = JSON.parse(body);
-        }
+  const contentType = rawResponse.headers.get("Content-Type");
 
-        if (typeof body === "object") {
-          // attempt to use better and human-friendly error messages
-          if (typeof body.message === "string") {
-            err = new Error(body.message);
-          } else if (
-            // attempt to utilize Stripe-inspired error messages
-            !Array.isArray(body) &&
-            body.error
-          ) {
-            if (body.error.message) {
-              err = new Error(body.error.message);
-            }
-            if (body.error.stack) {
-              err.stack = body.error.stack;
-            }
-            if (body.error.code) {
-              err.code = body.error.code;
-            }
-            if (body.error.param) {
-              err.param = body.error.param;
-            }
-          } else if (body.errors) {
-            err.errors = body.errors
-          }
-        }
-      } catch (e) {
-        err = e;
+  if (contentType && contentType.includes("application/json")) {
+    try {
+      if (typeof rawResponse.json === "function") {
+        body = await rawResponse.json();
+      } else {
+        body = await rawResponse.text();
+        body = JSON.parse(body);
       }
+    } catch (err) {
+      error = err;
     }
   }
-  return { body, err };
+
+  return { body, error }
 }
 
 export default async function createResponse(rawResponse, fRequest) {
-  const fResponse = {
-    body: null,
-    path: fRequest.path,
-    headers: {},
-    err: null,
-    ok: false,
-    raw: null,
-    request: fRequest,
-    status: null,
-    statusText: null,
-    type: null,
-    url: null
-  };
+  const { body, error } = await parseResponse(rawResponse);
 
-  if (rawResponse instanceof Response) {
-    fResponse.ok = rawResponse.ok;
-    fResponse.raw = rawResponse;
-    fResponse.status = rawResponse.status;
-    fResponse.statusText = rawResponse.statusText;
-    fResponse.type = rawResponse.type;
-    fResponse.url = rawResponse.url;
+  return new Proxy(rawResponse, {
+    get(target, key) {
+      switch (key) {
+        case "request":
+          return fRequest;
 
-    for (const [key, value] of rawResponse.headers) {
-      fResponse.headers[key] = value;
-    }
+        case "body":
+          return body;
 
-    const { body, err } = await parseResponse(
-      rawResponse,
-      rawResponse.headers.get("Content-Type")
-    );
+        case "error":
+          return error;
 
-    if (err) {
-      fResponse.err = err;
-    }
-    fResponse.body = body;
-  } else {
-    fResponse.ok = false
-    fResponse.err = rawResponse
-    statusText: rawResponse.message || null
-  }
-  return fResponse;
+        case "raw":
+          return rawResponse;
+
+        default:
+          return target[key];
+      }
+    },
+  });
 }
