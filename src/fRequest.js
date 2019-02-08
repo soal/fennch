d633646850e0d8b951d708b3c8b055d4ec33ba26
soal <sorrow.about.alice@pm.me>
2019-02-08 15:52:40 +0300

    Request creation experiments

--- src/fRequest.js
+++ src/fRequest.js
@@ -119,7 +119,9 @@ export default function createRequest(config) {
   if (config instanceof Request) {
     if (config.raw) {
       return makeProxy(config.raw.clone(), config.abortController)
-    };
+    } else {
+      return makeProxy(config.clone())
+    }
   }
 
   let {
