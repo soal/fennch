2eeccabdf31dda4af8e388b0990b69d88f02df89
soal <sorrow.about.alice@pm.me>
2019-02-08 16:24:30 +0300

    Experiments with requests cloning

--- src/fRequest.js
+++ src/fRequest.js
@@ -128,11 +128,7 @@ function makeProxy(rawRequest, abortController) {
 }
 export default function createRequest(config) {
   if (config instanceof Request) {
-    if (config.raw) {
-      return makeProxy(config.raw.clone(), config.abortController)
-    } else {
-      return makeProxy(config.clone())
-    }
+    return makeProxy(config.clone(), config.abortController)
   }
 
   let {
